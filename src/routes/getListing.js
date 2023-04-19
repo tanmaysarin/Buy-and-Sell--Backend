0be80e1f5ce8950776f66aaf0553a85aa938576b
Tanmay Sarin <tanmaysarin@gmail.com>
2023-04-19 01:50:57 -0500

    basic server side setup complete

--- src/routes/getListing.js
+++ src/routes/getListing.js
@@ -1,3 +1,5 @@
+import { Boom } from "@hapi/boom";
+
 import { fakeListings } from "./fake-data";
 
 export const getListingRoute = {
@@ -5,6 +7,12 @@ export const getListingRoute = {
     path: '/api/listings/{id}',
     handler: (req, h) => {
         const id = req.params.id;
-        return fakeListings.find(listing => listing.id === id);
+        const listing =  fakeListings.find(listing => listing.id === id);
+
+        if(!listing){
+            throw Boom.notFound(`Listing does not exist with if ${id}`);
+        }else{
+            return listing;
+        }
     }
 }
